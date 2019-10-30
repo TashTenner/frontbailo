32fba046af5f03b8d1b63e1b5649b03bb256e8bd
Tash Tenner <tashfonetikas@gmail.com>
2019-10-30 20:25:50 +0100

    closes: #11 add component map

--- src/App.js
+++ src/App.js
@@ -1,44 +1,78 @@
 import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-// import MapTwo from "./views/map/MapTwo";
-import MapThree from "./views/map/MapThree";
-// import Mapz from "./views/map/MapFour";
-// import CityPin from "./views/map/City-pen";
+import "./App.css";
+import MapHome from "./views/map/MapHome";
+// import About from "./components/About";
+
+// import Navbar from "./components/Navbar";
+
+// import Projects from "./components/Projects";
+// import { projects as Projects } from "./components/Projects";
+// import ProjectDetails from "./components/ProjectDetails";
+
+// import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
-  state = { listOfVenues: [] };
-
-  componentDidMount() {
-    axios.get("http://localhost:3001/api/venues").then(response => {
-      this.setState({ listOfVenues: response.data });
-      console.log(this.state.listOfVenues);
-    });
-  }
-
   render() {
-    // console.log(this.state.listOfVenues.listOfVenues);
     return (
       <div>
-        <div>
-          {this.state.listOfVenues.length > 0 &&
-            this.state.listOfVenues.map(venue => {
-              console.log("VENUE: ", venue);
-              return (
-                <div key={venue._id}>
-                  <Link to={`/venues/${venue._id}`}>
-                    <h3>{venue.dayOfWeek}</h3>
-                    {venue.coordinates ? <p>{venue.coordinates.lat}</p> : null}
-                    {venue.coordinates ? <p>{venue.coordinates.lng}</p> : null}
-                  </Link>
-                </div>
-              );
-            })}
-        </div>
-        <MapThree />
+        <MapHome />
       </div>
+      // <div className="App">
+      //   <Navbar />
+      //   <Switch>
+      //     <Route exact path="/" component={Home} />
+      //     <Route path="/about" component={About} />
+      //     <Route exact path="/projects" component={Projects} />
+      //     <Route exact path="/projects/:id" component={ProjectDetails} />
+      //   </Switch>
+      // </div>
     );
   }
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import axios from "axios";
+// import { Link } from "react-router-dom";
+// // import MapTwo from "./views/map/MapTwo";
+// import MapThree from "./views/map/MapThree";
+// // import Mapz from "./views/map/MapFour";
+// // import CityPin from "./views/map/City-pen";
+
+// class App extends Component {
+//   state = { listOfVenues: [] };
+
+//   componentDidMount() {
+//     axios.get("http://localhost:3001/api/venues").then(response => {
+//       this.setState({ listOfVenues: response.data });
+//       console.log(this.state.listOfVenues);
+//     });
+//   }
+
+//   render() {
+//     // console.log(this.state.listOfVenues.listOfVenues);
+//     return (
+//       <div>
+//         <div>
+//           {this.state.listOfVenues.length > 0 &&
+//             this.state.listOfVenues.map(venue => {
+//               console.log("VENUE: ", venue);
+//               return (
+//                 <div key={venue._id}>
+//                   <Link to={`/venues/${venue._id}`}>
+//                     <h3>{venue.dayOfWeek}</h3>
+//                     {venue.coordinates ? <p>{venue.coordinates.lat}</p> : null}
+//                     {venue.coordinates ? <p>{venue.coordinates.lng}</p> : null}
+//                   </Link>
+//                 </div>
+//               );
+//             })}
+//         </div>
+//         <MapThree />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/views/map/components/MapPin.js
+++ src/views/map/components/MapPin.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
@@ -10,7 +10,7 @@ const pinStyle = {
   stroke: "none"
 };
 
-export default class CityPin extends PureComponent {
+export default class MapPin extends Component {
   render() {
     const { size = 20, onClick } = this.props;
 
