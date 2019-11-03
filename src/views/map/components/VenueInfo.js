ba691b7efaa89d81c1243b8a409a9e344cdb8ba2
Tash Tenner <tashfonetikas@gmail.com>
2019-11-03 15:31:39 +0100

    closes #24 venue more details

--- src/views/map/components/VenueInfo.js
+++ src/views/map/components/VenueInfo.js
@@ -1,23 +1,47 @@
 import React, { PureComponent } from "react";
+import { Link } from "react-router-dom";
+// import venueService from "../../../services/venueService";
+
+// import VenueLongDetail from "../venues/VenueLongDetail";
 
 export default class VenueInfo extends PureComponent {
   render() {
-    // const {info} = this.props;
-    // const displayName = `${info.city}, ${info.state}`;
+    const { info } = this.props;
+    const displayName = `${info.properties.name}`;
+    // const venueId = `${info.venue._id}`;
+
+    // const getVenueId = id => {
+    //   const theVenueId = oneVenue => {
+    //     return oneVenue._id === id;
+    //   };
+    //   return venueService.getVenueById(theVenueId);
+    // };
+
+    // const { params } = this.props.match;
+    // const foundVenueId = getVenueId(params.id);
 
     return (
       <div>
-        hola
-        {/* <div>
-          {displayName} |{' '}
-          <a
-            target="_new"
-            href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${displayName}`}
-          >
-            Wikipedia
-          </a>
+        <div>
+          {/* <div>
+            <ul>
+              {foundVenueId.properties.name.map((_id, index) => {
+                const venue = getVenueId(_id).properties.name;
+                return (
+                  <li key={`${_id}-${index}`}>
+                    <Link to={`/${_id}`}>{venue}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div> */}
+          <Link to={`/venue/${info._id}`}>{displayName}</Link>
+          {/* <Route path="/:id" component={VenueLongDetail} /> */}
+          {/* <a target="_new" href={`http://localhost:3000/milonga/${venueId}`}>
+            {displayName}
+          </a> */}
         </div>
-        <img width={240} src={info.image} /> */}
+        {/* <img width={240} src={info.properties.mainPhoto} alt="" /> */}
       </div>
     );
   }
