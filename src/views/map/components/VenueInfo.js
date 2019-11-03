44b3ac74655117721469f9b01b63f9c356c882bf
Tash Tenner <tashfonetikas@gmail.com>
2019-11-03 15:43:36 +0100

    closes #26 update popup venue short info

--- src/views/map/components/VenueInfo.js
+++ src/views/map/components/VenueInfo.js
@@ -1,45 +1,23 @@
 import React, { PureComponent } from "react";
 import { Link } from "react-router-dom";
-// import venueService from "../../../services/venueService";
-
-// import VenueLongDetail from "../venues/VenueLongDetail";
 
 export default class VenueInfo extends PureComponent {
   render() {
     const { info } = this.props;
     const displayName = `${info.properties.name}`;
-    // const venueId = `${info.venue._id}`;
-
-    // const getVenueId = id => {
-    //   const theVenueId = oneVenue => {
-    //     return oneVenue._id === id;
-    //   };
-    //   return venueService.getVenueById(theVenueId);
-    // };
-
-    // const { params } = this.props.match;
-    // const foundVenueId = getVenueId(params.id);
 
     return (
       <div>
         <div>
-          {/* <div>
-            <ul>
-              {foundVenueId.properties.name.map((_id, index) => {
-                const venue = getVenueId(_id).properties.name;
-                return (
-                  <li key={`${_id}-${index}`}>
-                    <Link to={`/${_id}`}>{venue}</Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div> */}
           <Link to={`/venue/${info._id}`}>{displayName}</Link>
-          {/* <Route path="/:id" component={VenueLongDetail} /> */}
-          {/* <a target="_new" href={`http://localhost:3000/milonga/${venueId}`}>
-            {displayName}
-          </a> */}
+        </div>
+        <div>
+          <div>{info.properties.address}</div>
+          <div>
+            {" "}
+            {info.properties.startTime}-{info.properties.endTime}
+          </div>
+          <div> {info.properties.price}</div>
         </div>
         {/* <img width={240} src={info.properties.mainPhoto} alt="" /> */}
       </div>
